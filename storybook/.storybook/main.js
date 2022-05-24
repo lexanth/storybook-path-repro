300cd90a1d8b170c5a71453ef1fe3ff3b79783ec
Alex Anthony <alex.anthony@oaknorth.com>
2022-05-24 13:25:40 +0100

    Repro

---
+++ storybook/.storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: [
+    '../../*/stories/**/*.stories.mdx',
+    '../../*/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  features: {
+    babelModeV7: true,
+    emotionAlias: false,
+    storyStoreV7: true,
+  },
+}
